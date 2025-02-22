dec2d61e36b9e3471e9a58af68e88acff9a8b844
holabayor <liasu.olabayo@gmail.com>
2025-02-22 18:32:56 +0100

    Refactor Storage class to manage events in memory, removing file system dependencies

--- src/data/storage.ts
+++ src/data/storage.ts
@@ -1,41 +1,58 @@
-import fs from 'fs';
+// import fs from 'fs';
 import { EventData } from '../types';
 
-const EVENTS_FILE = './events.json';
+// const EVENTS_FILE = './events.json';
+
+// export default class Storage {
+//   private eventsFile: string;
+
+//   constructor(filePath: string = EVENTS_FILE) {
+//     this.eventsFile = filePath;
+
+//     if (!fs.existsSync(this.eventsFile)) {
+//       fs.writeFileSync(this.eventsFile, JSON.stringify([]));
+//     }
+//   }
+
+//   public getEvents(): EventData[] {
+//     try {
+//       const data = fs.readFileSync(this.eventsFile, { encoding: 'utf-8' });
+//       return JSON.parse(data) as EventData[];
+//     } catch (error) {
+//       console.error('Error reading events file', error);
+//       return [];
+//     }
+//   }
+
+//   public saveEvents(events: EventData[]): void {
+//     fs.writeFileSync(this.eventsFile, JSON.stringify(events));
+//   }
+
+//   public addEvent(event: EventData): void {
+//     const events = this.getEvents();
+//     events.push(event);
+//     this.saveEvents(events);
+//   }
+//   public deleteEvent(eventId: string): void {
+//     const events = this.getEvents();
+//     const updatedEvents = events.filter((event) => event.id !== eventId);
+//     this.saveEvents(updatedEvents);
+//   }
+// }
+
 
 export default class Storage {
-  private eventsFile: string;
-
-  constructor(filePath: string = EVENTS_FILE) {
-    this.eventsFile = filePath;
-
-    if (!fs.existsSync(this.eventsFile)) {
-      fs.writeFileSync(this.eventsFile, JSON.stringify([]));
-    }
-  }
+  private events: EventData[] = [];
 
   public getEvents(): EventData[] {
-    try {
-      const data = fs.readFileSync(this.eventsFile, { encoding: 'utf-8' });
-      return JSON.parse(data) as EventData[];
-    } catch (error) {
-      console.error('Error reading events file', error);
-      return [];
-    }
-  }
-
-  public saveEvents(events: EventData[]): void {
-    fs.writeFileSync(this.eventsFile, JSON.stringify(events));
+    return this.events;
   }
 
   public addEvent(event: EventData): void {
-    const events = this.getEvents();
-    events.push(event);
-    this.saveEvents(events);
+    this.events.push(event);
   }
+
   public deleteEvent(eventId: string): void {
-    const events = this.getEvents();
-    const updatedEvents = events.filter((event) => event.id !== eventId);
-    this.saveEvents(updatedEvents);
+    this.events = this.events.filter(event => event.id !== eventId);
   }
 }
